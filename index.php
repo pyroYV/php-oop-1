f7d715c74d435399cd9b3f0236ff782268f5c575
pyroYV <simonerizzo.pvt@gmail.com>
2022-09-02 14:43:04 +0200

    Feat: Print in page

--- index.php
+++ index.php
@@ -31,6 +31,10 @@ class Movie{
         return $this->image;
     }
 }
+
+$movieOne = new Movie('Back to the Future','Robert Zemeckis','https://www.themoviedb.org/t/p/w500/fNOH9f1aA7XRTzl1sAOx9iF553Q.jpg');
+
+$movieTwo = new Movie('Interstella 5555','Kazuhisa Takenouchi','https://m.media-amazon.com/images/I/81ZrcwqeAbL._AC_SY445_.jpg');
 ?>
 
 <!DOCTYPE html>
@@ -42,6 +46,27 @@ class Movie{
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>
+        Movies
+    </h1>
+
+    <ul>
+        <li>
+            <?php echo $movieOne->getTitle() ?>
+        </li>
+        <li>
+            <?php echo $movieOne->getDirector() ?>
+        </li>
+         <img src=" <?php echo $movieOne->getImage() ?>" alt="">
+    </ul>
+    <ul>
+        <li>
+            <?php echo $movieTwo->getTitle() ?>
+        </li>
+        <li>
+            <?php echo $movieTwo->getDirector() ?>
+        </li>
+         <img src=" <?php echo $movieTwo->getImage() ?>" alt="">
+    </ul>
 </body>
 </html>
